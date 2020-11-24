53f47c31c1bfeccca519b525776ac1948b304517
Jorge Roldan Barrio <Jorge.RoldanBarrio@zooplus.com>
2020-11-24 15:38:39 +0100

    Create List component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Loading from './components/Loading'
+import React, { Component } from 'react';
+import List from './components/List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Loading/>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (<List/>);
+  }
 }
 
 export default App;
