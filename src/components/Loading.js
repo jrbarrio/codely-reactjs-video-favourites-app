d27de03666bb7d992c3dbab3e63058a50f7a04f3
Jorge Roldan Barrio <Jorge.RoldanBarrio@zooplus.com>
2020-11-24 16:42:21 +0100

    Create Header and Item components

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import PropTypes from 'prop-types';
 
 class Loading extends Component {
-
+    
     render() {
-        return (<div className="loader">{this.props.message}</div>);
+        return (<div className="loader" ref={el => this.$loading = el} >{this.props.message}</div>);
     }
 }
 
