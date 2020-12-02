306dfd06ef6cf9873c5f0c6e248985ab2a0d4bdc
Jorge Roldan Barrio <Jorge.RoldanBarrio@zooplus.com>
2020-12-02 16:15:13 +0100

    Adding visual effects

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,15 +1,45 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import PropTypes from 'prop-types';
+import Typed from 'typed.js';
 
-class Loading extends Component {
-    
+class Loading extends PureComponent {
+
+    componentDidMount() {
+        const speed = this.props.speed || 90;
+        // this.typed = new Typed(this.$loading, {
+        //     strings: [this.props.message],
+        //     typeSpeed: parseInt(speed)
+        // });
+        fadeIn(this.$loading)
+    }
+    componentWillUnmount() {
+        //this.typed.destroy();
+    }
     render() {
+        // return (<div className="loader" ref={el => this.$loading = el} ></div>);
         return (<div className="loader" ref={el => this.$loading = el} >{this.props.message}</div>);
     }
 }
 
 Loading.propTypes = {
-    message: PropTypes.string.isRequired
+    message: PropTypes.string.isRequired,
+    speed: PropTypes.number
 };
+
+function fadeIn(el) {
+    el.style.opacity = 0;
+
+    var last = +new Date();
+    var tick = function() {
+        el.style.opacity = +el.style.opacity + (new Date() - last) / 400;
+        last = +new Date();
+
+        if (+el.style.opacity < 1) {
+            (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+        }
+    };
+
+    tick();
+}
 
 export default Loading;
